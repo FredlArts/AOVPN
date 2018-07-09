4233bedc6e6d705864822b0a09fa1ff6ad8d7857
FredlArts <22217233+FredlArts@users.noreply.github.com>
2018-07-09 17:15:58 +0200

    Hopefully final customizatio

--- Run_NPSDeployment.ps1
+++ Run_NPSDeployment.ps1
@@ -2,8 +2,8 @@ Connect-AzureRmAccount
 
 Select-AzureRmSubscription -SubscriptionName "AGRANA EA-NE_163"
 
-New-AzureRmResourceGroup -Name AOVPN-Test -Location "West Europe"
-
-New-AzureRmResourceGroupDeployment -Name new-NPSVMSet -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSet.json -TemplateParameterUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSetParam.json
+New-AzureRmResourceGroup -Name "AZWE_AOVPN" -Location "West Europe"
 
 New-AzureRmResourceGroupDeployment -Name new-RASVMNSG -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMNSG.JSON -TemplateParameterUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMNSGParam.json
+
+New-AzureRmResourceGroupDeployment -Name new-NPSVMSet -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSet.json -TemplateParameterUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSetParam.json
