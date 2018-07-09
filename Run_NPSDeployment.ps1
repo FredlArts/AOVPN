edba92840da547a452b6a02a42a15071e8b58530
FredlArts <22217233+FredlArts@users.noreply.github.com>
2018-07-09 17:27:50 +0200

    So much to do

--- Run_NPSDeployment.ps1
+++ Run_NPSDeployment.ps1
@@ -4,6 +4,8 @@ Select-AzureRmSubscription -SubscriptionName "AGRANA EA-NE_163"
 
 New-AzureRmResourceGroup -Name "AZWE_AOVPN" -Location "West Europe"
 
-New-AzureRmResourceGroupDeployment -Name new-RASVMNSG -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMNSG.JSON -TemplateParameterUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMNSGParam.json
+New-AzureRmResourceGroupDeployment -Name new-RASVMNSG -ResourceGroupName AZWE_AOVPN -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMNSG.JSON -TemplateParameterUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMNSGParam.json
 
-New-AzureRmResourceGroupDeployment -Name new-NPSVMSet -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSet.json -TemplateParameterUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSetParam.json
+New-AzureRmResourceGroupDeployment -Name new-RASVMSet -ResourceGroupName AZWE_AOVPN -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMSet.json -TemplateParameterUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMSetParam.json
+
+New-AzureRmResourceGroupDeployment -Name new-NPSVMSet -ResourceGroupName AZWE_AOVPN -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSet.json -TemplateParameterUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSetParam.json
