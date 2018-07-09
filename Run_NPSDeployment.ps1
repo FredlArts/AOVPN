99dc5fb1ab5262531dce1964d8f2ad636baeda91
FredlArts <22217233+FredlArts@users.noreply.github.com>
2018-07-09 14:25:07 +0200

    Updated

--- Run_NPSDeployment.ps1
+++ Run_NPSDeployment.ps1
@@ -4,8 +4,8 @@ Select-AzureRmSubscription -SubscriptionName "AGRANA EA-NE_163"
 
 New-AzureRmResourceGroup -Name AOVPN-Test -Location "West Europe"
 
+New-AzureRmResourceGroupDeployment -Name new-NPSVMSet -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSet.json -TemplateParameterFile https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSetParam.json
+
 New-AzureRmResourceGroupDeployment -Name new-RASVMSet -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMSet.json
 
 New-AzureRmResourceGroupDeployment -Name new-RASVMNSG -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMNSG.json -TemplateParameterFile https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMNSGParam.json
-
-New-AzureRmResourceGroupDeployment -Name new-NPSVMSet -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSet.json -TemplateParameterFile https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSetParam.json
