d3641be4f127120ed6e95165d9271f4f3bb9df78
FredlArts <22217233+FredlArts@users.noreply.github.com>
2018-07-09 16:27:47 +0200

    Yes

--- Run_NPSDeployment.ps1
+++ Run_NPSDeployment.ps1
@@ -6,6 +6,4 @@ New-AzureRmResourceGroup -Name AOVPN-Test -Location "West Europe"
 
 New-AzureRmResourceGroupDeployment -Name new-NPSVMSet -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSet.json -TemplateParameterUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-NPSVMSetParam.json
 
-New-AzureRmResourceGroupDeployment -Name new-RASVMSet -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMSet.json -TemplateParameterUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMSetParam.json
-
 New-AzureRmResourceGroupDeployment -Name new-RASVMNSG -ResourceGroupName AOVPN-Test -TemplateUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMNSG.JSON -TemplateParameterUri https://raw.githubusercontent.com/fredlarts/AOVPN/master/new-RASVMNSGParam.json
